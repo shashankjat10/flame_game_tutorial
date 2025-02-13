c271619aa71b26f1f954725434bf322811df77a9
shashankjat10 <shashankjat10@gmail.com>
2025-02-13 09:04:10 +0530

    fixing the platform block class with on load and update methods

--- lib/objects/platform_block.dart
+++ lib/objects/platform_block.dart
@@ -1,1 +1,27 @@
-class PlatformBlock {}
+import 'package:flame/components.dart';
+import 'package:flame_game_tutorial/ember_quest.dart';
+
+class PlatformBlock extends SpriteComponent
+    with HasGameReference<EmberQuestGame> {
+  final Vector2 gridPosition;
+  double xOffset;
+
+  PlatformBlock({
+    required this.gridPosition,
+    required this.xOffset,
+  }) : super(size: Vector2.all(64), anchor: Anchor.bottomLeft);
+
+  final Vector2 velocity = Vector2.zero();
+
+  @override
+  void onLoad() {
+  }
+
+  @override
+  void update(double dt) {
+    velocity.x = game.objectSpeed;
+    position += velocity * dt;
+    if (position.x < -size.x) removeFromParent();
+    super.update(dt);
+  }
+}
