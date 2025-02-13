2384a99f9d1732f04c0ed2f107dffa51283ad0b6
newt0n <newt0n@newt0ns-MacBook-Air.local>
2025-02-13 08:35:18 +0530

    adding ember to the game and adding other objects

--- lib/ember_quest.dart
+++ lib/ember_quest.dart
@@ -1,7 +1,11 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame_game_tutorial/actors/ember.dart';
 
 class EmberQuestGame extends FlameGame{
   EmberQuestGame();
+
+  late EmberPlayer _ember;
 
   @override
   Future<void> onLoad() async {
@@ -15,5 +19,12 @@ class EmberQuestGame extends FlameGame{
       'star.png',
       'water_enemy.png',
     ]);
+
+    // Everything in this tutorial assumes that the position
+    // of the `CameraComponent`s viewfinder (where the camera is looking)
+    // is in the top left corner, that's why we set the anchor here.
+    camera.viewfinder.anchor = Anchor.topLeft;
+    _ember = EmberPlayer(position: Vector2(128, canvasSize.y - 70));
+    world.add(_ember);
   }
 }
